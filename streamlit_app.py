3952e0fff50f06c5f907b6090547d258366ba62d
Diah Amelia <87484500+diahamelia@users.noreply.github.com>
2022-07-20 00:11:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import panda as pd
 
+st.header("Alan Jones")
+st.subheader("Writer and Developer")
+st.write("some descriptive text goes in here")
+with st.container():
+   image_col, text_col = st.columns(1,2):
+   with image_col:
+      st.image(image)
+   with text_col:
+      st.write(text)
+with st.container():
+   image_col, text_col = st.columns(1,2):
+   with image_col:
+      st.image(image2)
+   with text_col:
+      st.write(text2)
