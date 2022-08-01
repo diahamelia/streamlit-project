f6a1048df9ddfaeaf1e250b7aeb12df559c6671e
Diah Amelia <87484500+diahamelia@users.noreply.github.com>
2022-08-01 12:30:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.title('This is a title')
 st.header("Cybersecurity")
 st.subheader("How Often You Have to Change Your Passwords")
 st.write("Password is the................")
