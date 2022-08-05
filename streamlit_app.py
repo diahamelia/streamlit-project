fb76473a728b24ff31c3a099a372290d4043bba5
Diah Amelia <87484500+diahamelia@users.noreply.github.com>
2022-08-05 15:40:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,7 @@ def cybercrime_type():
     st.write("# Apa aja sih tipe-tipe Cybercrime?")
     st.markdown(
         """
-        This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!
+        Tipe cybercrime
 """
     )
 
@@ -57,16 +55,7 @@ def case_study():
     st.write("# Contoh kasus Cybercrime")
     st.markdown(
         """
-        Teknologi yang terus berkembang, tak lantas membebaskan kita dari berbagai jenis kejahatan di dunia maya atau cybercrime. 
-        Pengertian cybercrime adalah sebuah kejahatan menggunakan komputer, Internet, dan sebagainya.
-        Umumnya, jenis kejahatan ini dimanfaatkan oleh para pelakunya untuk mendapatkan informasi secara ilegal, memanipulasi data, 
-        dan berbagai tindakan kejahatan virtual lainnya guna mendapatkan keuntungan. 
-        Terus, apa sih yang dimaksud dengan cybercrime?.
-        Oke, jadi Cybercrime adalah uatu bentuk kejahatan virtual dengan memanfaatkan perangkat komputer yang terhubung dengan jaringan Internet. 
-        Tindakan tersebut tentunya melanggar hukum, sebab dapat menimbulkan kerugian bagi orang lain.
-        Menyadur dari buku Etika Profesi Informatika oleh Muhammad Ridha Albaar, kejahatan siber pertama kali ditemukan pada 1988 dengan istilah cyber attack. 
-        Saat itu, seorang mahasiswa berhasil menciptakan sebuah worm atau virus yang dapat menyerang program komputer dan dapat mematikan sekitar 10 persen 
-        dari seluruh jumlah komputer di dunia yang terhubung di Internet.
+        Kalian tau gak sih .
     """
     )
 
